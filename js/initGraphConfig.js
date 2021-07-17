69067fdb6faa47c2db413cce4a8a9ade8a51d521
Chris <christoph.buss@uni-oldenburg.de>
2021-07-17 02:21:11 +0200

    Finally fixed the CSS Grid Layout Added support for Auto Layouting and Fitting Added Labels Added Basic Heatmap Color Scale

--- js/initGraphConfig.js
+++ js/initGraphConfig.js
@@ -6,9 +6,43 @@ var cy = cytoscape({
             selector: 'node' ,
             style: {
                 shape: 'hexagon',
-                label: 'data(id)'
+                label: 'data(antCount)'
+            }
+        },
+        {
+            selector: 'edge' ,
+            style: {
+                'label' : 'data(pheromoneCount)',
+                'line-color': 'mapData(pheromoneCount, 0, 100, #CEF1DE, #226D41)'
             }
         }
 
     ],
+    // initial viewport state:
+    zoom: 1,
+    pan: { x: 0, y: 0 },
+
+    // interaction options:
+    minZoom: 1,
+    maxZoom: 4,
+    zoomingEnabled: true,
+    userZoomingEnabled: true,
+    panningEnabled: true,
+    userPanningEnabled: true,
+    boxSelectionEnabled: true,
+    selectionType: 'single',
+    touchTapThreshold: 8,
+    desktopTapThreshold: 4,
+    autolock: false,
+    autoungrabify: false,
+    autounselectify: false,
+
+    // rendering options:
+    headless: false,
+    styleEnabled: true,
+    hideEdgesOnViewport: false,
+    textureOnViewport: false,
+    motionBlur: false,
+    motionBlurOpacity: 0.2,
+    pixelRatio: 'auto'
 });
--- js/initTestGraph.js
+++ js/initTestGraph.js
@@ -1,36 +1,119 @@
 // Example Test Graph
 var eles = cy.add([
-    { group: 'nodes', data: { id: 'n0' }},
-    { group: 'nodes', data: { id: 'n1' }},
-    { group: 'nodes', data: { id: 'n2' }},
-    { group: 'nodes', data: { id: 'n3' }},
-    { group: 'nodes', data: { id: 'n4' }},
-    { group: 'nodes', data: { id: 'n5' }},
-    { group: 'nodes', data: { id: 'n6' }},
-    { group: 'edges', data: { id: 'e0', source: 'n0', target: 'n1' } },
-    { group: 'edges', data: { id: 'e1', source: 'n0', target: 'n2' } },
-    { group: 'edges', data: { id: 'e2', source: 'n0', target: 'n3' } },
-    { group: 'edges', data: { id: 'e3', source: 'n1', target: 'n4' } },
-    { group: 'edges', data: { id: 'e4', source: 'n2', target: 'n4' } },
-    { group: 'edges', data: { id: 'e5', source: 'n2', target: 'n4' } },
-    { group: 'edges', data: { id: 'e6', source: 'n2', target: 'n5' } },
-    { group: 'edges', data: { id: 'e7', source: 'n3', target: 'n5' } },
-    { group: 'edges', data: { id: 'e8', source: 'n4', target: 'n6' } },
-    { group: 'edges', data: { id: 'e9', source: 'n5', target: 'n6' } }
+    {group: 'nodes', data: {id: 'n0', antCount: '10'}},
+    {group: 'nodes', data: {id: 'n1', antCount: '20'}},
+    {group: 'nodes', data: {id: 'n2', antCount: '30'}},
+    {group: 'nodes', data: {id: 'n3', antCount: '40'}},
+    {group: 'nodes', data: {id: 'n4', antCount: '50'}},
+    {group: 'nodes', data: {id: 'n5', antCount: '60'}},
+    {group: 'nodes', data: {id: 'n6', antCount: '70'}},
+    {group: 'edges', data: {id: 'e0', source: 'n0', target: 'n1', pheromoneCount: 0}},
+    {group: 'edges', data: {id: 'e1', source: 'n0', target: 'n2', pheromoneCount: 10}},
+    {group: 'edges', data: {id: 'e2', source: 'n0', target: 'n3', pheromoneCount: 20}},
+    {group: 'edges', data: {id: 'e3', source: 'n1', target: 'n4', pheromoneCount: 30}},
+    {group: 'edges', data: {id: 'e4', source: 'n2', target: 'n4', pheromoneCount: 40}},
+    {group: 'edges', data: {id: 'e5', source: 'n2', target: 'n4', pheromoneCount: 50}},
+    {group: 'edges', data: {id: 'e6', source: 'n2', target: 'n5', pheromoneCount: 60}},
+    {group: 'edges', data: {id: 'e7', source: 'n3', target: 'n5', pheromoneCount: 70}},
+    {group: 'edges', data: {id: 'e8', source: 'n4', target: 'n6', pheromoneCount: 80}},
+    {group: 'edges', data: {id: 'e9', source: 'n5', target: 'n6', pheromoneCount: 90}}
 
 ]);
 
 let options = {
-    name: 'random',
+    name: 'cose',
 
-    fit: true, // whether to fit to viewport
-    padding: 30, // fit padding
-    boundingBox: undefined, // constrain layout bounds; { x1, y1, x2, y2 } or { x1, y1, w, h }
-    animate: false, // whether to transition the node positions
-    animationDuration: 500, // duration of animation in ms if enabled
-    animationEasing: undefined, // easing of animation if enabled
-    animateFilter: function ( node, i ){ return true; }, // a function that determines whether the node should be animated.  All nodes animated by default on animate enabled.  Non-animated nodes are positioned immediately when the layout starts
-    transform: function (node, position ){ return position; } // transform a given node position. Useful for changing flow direction in discrete layouts
+    // Called on `layoutready`
+    ready: function () {
+    },
+
+    // Called on `layoutstop`
+    stop: function () {
+    },
+
+    // Whether to animate while running the layout
+    // true : Animate continuously as the layout is running
+    // false : Just show the end result
+    // 'end' : Animate with the end result, from the initial positions to the end positions
+    animate: 'end',
+
+    // Easing of the animation for animate:'end'
+    animationEasing: undefined,
+
+    // The duration of the animation for animate:'end'
+    animationDuration: 500,
+
+    // A function that determines whether the node should be animated
+    // All nodes animated by default on animate enabled
+    // Non-animated nodes are positioned immediately when the layout starts
+    animateFilter: function (node, i) {
+        return true;
+    },
+
+
+    // The layout animates only after this many milliseconds for animate:true
+    // (prevents flashing on fast runs)
+    animationThreshold: undefined,
+
+    // Number of iterations between consecutive screen positions update
+    refresh: 20,
+
+    // Whether to fit the network view after when done
+    fit: true,
+
+    // Padding on fit
+    padding: 30,
+
+    // Constrain layout bounds; { x1, y1, x2, y2 } or { x1, y1, w, h }
+    boundingBox: undefined,
+
+    // Excludes the label when calculating node bounding boxes for the layout algorithm
+    nodeDimensionsIncludeLabels: true,
+
+    // Randomize the initial positions of the nodes (true) or use existing positions (false)
+    randomize: true,
+
+    // Extra spacing between components in non-compound graphs
+    componentSpacing: 400,
+
+    // Node repulsion (non overlapping) multiplier
+    nodeRepulsion: function (node) {
+        return 2048;
+    },
+
+    // Node repulsion (overlapping) multiplier
+    nodeOverlap: 4,
+
+    // Ideal edge (non nested) length
+    idealEdgeLength: function (edge) {
+        return 32;
+    },
+
+    // Divisor to compute edge forces
+    edgeElasticity: function (edge) {
+        return 32;
+    },
+
+    // Nesting factor (multiplier) to compute ideal edge length for nested edges
+    nestingFactor: 1.2,
+
+    // Gravity force (constant)
+    gravity: 1,
+
+    // Maximum number of iterations to perform
+    numIter: 1000,
+
+    // Initial temperature (maximum node displacement)
+    initialTemp: 1000,
+
+    // Cooling factor (how the temperature is reduced between consecutive iterations
+    coolingFactor: 0.99,
+
+    // Lower temperature threshold (below this point the layout will end)
+    minTemp: 1.0
 };
 
 cy.layout(options);
+cy.layout(options).run();
+cy.fit();
+cy.resize();
